e3ed4052e9ac06f4793ba5a88f1c118bb78f69ba
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-11-06 01:32:24 +0300

    gprivate: settingsa: Initial import
    Test: n/a (needs whole series)
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

---
+++ settingsa/src/com/google/android/settings/gestures/columbus/Stub.java
@@ -0,0 +1,13 @@
+/*
+    Copyright 2021 Acryl Project
+    Proprietary
+*/
+
+package com.google.android.settings.gestures.columbus;
+
+import com.android.settings.R;
+
+public class Stub {
+    public static final int[] ACTION_TITLE_RES_IDS = {R.string.columbus_setting_action_screenshot_title, R.string.columbus_setting_action_assistant_title, R.string.columbus_setting_action_play_pause_title, R.string.columbus_setting_action_overview_title, R.string.columbus_setting_action_notification_title, R.string.columbus_setting_action_launch_title};
+    public static final int[] ACTION_VALUE_RES_IDS = {R.string.columbus_setting_action_screenshot_value, R.string.columbus_setting_action_assistant_value, R.string.columbus_setting_action_play_pause_value, R.string.columbus_setting_action_overview_value, R.string.columbus_setting_action_notification_value, R.string.columbus_setting_action_launch_value};
+}
